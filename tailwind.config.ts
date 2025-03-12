bbbb4531a4e275db074a15ccb2d3a719f046513a
patrick <patrick@GTbank>
2025-03-12 09:55:32 +0300

    new font

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+        serif: ['var(--font-playfair)', 'serif'],
+        cursive: ['var(--font-tangerine)', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
